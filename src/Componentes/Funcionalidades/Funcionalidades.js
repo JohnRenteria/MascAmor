20a86a5f5c1d9a3470455c35f9340e6ade2e069d
JohnRenteria <jadherrenteria96@gmail.comm>
2023-11-28 22:31:41 -0500

    Creacion del 3 Componente

---
+++ src/Componentes/Funcionalidades/Funcionalidades.js
@@ -0,0 +1,25 @@
+import './Funcionalidades.css';
+import React from 'react';
+
+function Funcionalidades(){
+    return(
+    <div className="Conteiner-Principal">
+        <div className="Funcionalidades">
+            <div className="Funcion">
+                <a href='#'>VETERINARIA</a>
+            </div>
+            <div className="Funcion">
+                <a href='#'>PELUQUERIA</a>
+            </div>
+            <div className="Funcion">
+                <a href='#'>ADOPCIÓN</a>
+            </div>
+            <div className="Funcion">
+                <a href='#'>TIENDA ONLINE</a>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default Funcionalidades;
