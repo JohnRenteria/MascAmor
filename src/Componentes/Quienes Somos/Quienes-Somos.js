bfb13b0da89ef9459442caa2ee3a150af859db5a
JohnRenteria <jadherrenteria96@gmail.comm>
2023-11-28 22:49:17 -0500

    Creacion del 4 Componente

---
+++ src/Componentes/Quienes Somos/Quienes-Somos.js
@@ -0,0 +1,14 @@
+import './Quienes-Somos.css';
+import React from 'react';
+
+function QuienesSomos(){
+    return(
+        <div className="Quienes-Somos">
+            <h1>¿Quienes Somos?</h1>
+            <p>Somos personas amantes a los animales que garantizamos el cuidado integral de tus mascotas como si fueran nuestros hijos</p>
+            <h3>Contactenos...</h3>
+        </div>
+    );
+}
+
+export default QuienesSomos;
